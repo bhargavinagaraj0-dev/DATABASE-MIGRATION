592ca6ffb1b45dae885a4e57b9aedf282e8fc62b
bhargavinagaraj0-dev <bhargavinagaraj06@gmail.com>
2026-04-13 17:26:31 +0530

    Add files via upload

---
+++ Modified.sql
@@ -0,0 +1,27 @@
+
+CREATE TABLE customers (
+  customer_id SERIAL PRIMARY KEY,
+  customer_name varchar(45) NOT NULL,
+  customer_phno varchar(45) NOT NULL,
+  customer_address varchar(45) NOT NULL
+);
+
+INSERT INTO customers VALUES (101,'Ravi','8745321089','JP Nagar'), 
+(102,'priya','7845263001','HAL'), 
+(103,'sara','6324907452','B.Narayanapura'),
+(104,'Jhon','7456239016','4th block jpnagar'),
+(105,'smith','6664538791','8th cross bemal');
+
+
+
+CREATE TABLE orders (
+  order_id SERIAL PRIMARY KEY,
+  order_amount decimal(10,2) NOT NULL,
+  customer_id int DEFAULT NULL,
+  CONSTRAINT customer_id FOREIGN KEY (customer_id) REFERENCES customers (customer_id)
+);
+
+INSERT INTO orders VALUES (1,1500.30,102),
+(2,800.50,104),
+(3,1000.00,103),
+(4,1800.70,105);
